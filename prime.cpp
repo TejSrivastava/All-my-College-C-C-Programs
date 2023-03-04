d751552df368cab05cfee2b2accc780583686b6d
Tejas Srivastava <103350064+TejSrivastava@users.noreply.github.com>
2023-03-04 20:02:13 +0530

    Added

---
+++ prime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num,count=0;
+    cin>>num;
+
+    for(int i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+
+    if(count==2)
+    {
+        cout<<"Prime";
+    }
+    else{
+        cout<<"Not Prime";
+    }
+}
